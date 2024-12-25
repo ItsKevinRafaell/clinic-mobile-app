5e72f989005861ea40805041a8dc1f7efb35580b
ItsKevinRafaell <itskevin.rafael@gmail.com>
2024-12-25 15:41:57 +0700

    merge:slicing template and library

---
+++ android/app/src/main/kotlin/com/example/flutter_clinicmobile_app_kevin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_clinicmobile_app_kevin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_clinicmobile_app_kevin/presentation/auth/pages/onboarding_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'core/constants/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: AppColors.primary,
+      statusBarBrightness: Brightness.dark,
+    ));
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+        useMaterial3: true,
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        appBarTheme: AppBarTheme(
+          color: AppColors.white,
+          elevation: 0,
+          titleTextStyle: GoogleFonts.poppins(
+            color: AppColors.primary,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+          iconTheme: const IconThemeData(
+            color: AppColors.primary,
+          ),
+        ),
+      ),
+      home: const OnboardingPage(),
+    );
+  }
+}
